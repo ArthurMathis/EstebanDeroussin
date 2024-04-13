d09b1d15b34a1044669b772ed9321708bca0ef37
ArthurMathis <mathis.arthur68@gmail.com>
2024-04-13 19:43:01 +0200

    sychronisation des animations

--- scripts/view.js
+++ scripts/view.js
@@ -12,6 +12,14 @@ const calendrier = document.getElementById('calendrier');
 
 calendrier.textContent = localDate.toString();
 
-// Ajout des lignes dynamiques
+// Ajout de l'animation du rectangle
 
-const lignes = new AnimateLignes('.lignes');
+inView('.rectangle').on('enter', function(c){
+    c.classList.add('active');
+
+    // Ajout d'une pause de 250ms avant de déclarer les lignes dynamiques
+    setTimeout(() => {
+        // Ajout des lignes dynamiques
+        const lignes = new AnimateLignes('.lignes');
+    }, 100);
+});
