b095089ac97a5aa6b8e99efdbbdaacf3b064d26a
ArthurMathis <mathis.arthur68@gmail.com>
2024-04-14 11:37:37 +0200

    Ajout du panneau de développemnt

--- scripts/view.js
+++ scripts/view.js
@@ -31,3 +31,16 @@ inView('.rectangle').on('enter', function(c){
         }, 850);
     }, 600);
 });
+
+// Ajout de la notification de développement
+const dev_buttons = document.querySelectorAll('.dev_button');
+const dev_notification = document.getElementById('developpement');
+dev_buttons.forEach(button => {
+    button.addEventListener('click', c => {
+        dev_notification.style.display = 'flex';
+        const close_button = document.getElementById('dev_notif_close_icon');
+        close_button.addEventListener('click', () => {
+            dev_notification.style.display = 'none';
+        });
+    });
+});
